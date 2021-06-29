63bcb83fdb7936420dd4850585860f21e7fbf2e3
drrobb04 <drrobb04@gmail.com>
2021-06-28 19:46:19 -0600

    read data, takeout not working event listener

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -6,19 +6,23 @@ console.log(data_final);
 var tbody = d3.select("tbody");
 
 //make table function
+function makeTable(){
+    
+    //console log data to make sure it is there
+    data_final.forEach(function(row) {
+        console.log(row);
+        });
 
+};
 
 //filter data function (index.html, #datetime)
 function filterData(){
     var date = d3.select("#datetime").property("value");
     filterData = data_final.filter(row => row.datetime === date);
-}
-
-
+};
 
 //event listener for clicks (index.html, #filter-btn)
-
-
+//d3.selectAll("filter-btn").on("click");
 
 //call make table function
 makeTable(data);
